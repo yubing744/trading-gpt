3f9d18d493149d50be652bfbb63ea2d683e1d702
Owen Wu <yubing744@163.com>
2024-05-19 20:32:06 +0800

    feat: update prompt

--- pkg/prompt/prompt.go
+++ pkg/prompt/prompt.go
@@ -11,26 +11,21 @@ Trading strategy:
 {{.Strategy}}
 
 {{- if .StrategyAttentionPoints}}
-Trading strategy points of attention:
+Strategy points of attention:
 {{- range $index, $item := .StrategyAttentionPoints}}
 {{add $index 1}}. {{$item}}
 {{- end}}
 {{- end}}
 
-Performance Evaluation:
-1. Continuously review and analyze your actions to ensure you are performing to the best of your abilities.
-2. Every command has a cost, so be smart and efficient. Aim to complete tasks in the least number of steps.
-3. Command parameters do not support variables or expressions. Please fill them in after calculating them step by step.
-
 Constraints:
 1. Exclusively use the commands listed in double quotes e.g. "command name"
 2. The command's parameters only support strings. If the parameters are of other types, please convert them all to strings.
-3、Be careful not to open positions repeatedly. If you need to adjust the take profit and stop loss, please use the exchange.update_position command.
-4、Please remember to only reply in JSON format, no other explanation is required.
-5、The returned JSON format does not support comments
-6、The analyze statement can be very long to ensure that the reasoning process of the analysis is rigorous.
+3. Command parameters do not support variables or expressions. Please fill them in after calculating them step by step.
+4、Be careful not to open positions repeatedly. If you need to adjust the take profit and stop loss, please use the exchange.update_position command.
+5、The analyze statement can be very long to ensure that the reasoning process of the analysis is rigorous.
+6、The returned JSON format does not support comments
 
-You should only respond in JSON format as described below 
+You should only respond in JSON format as described below, no other explanation is required
 Response Format: 
 {
     "thoughts": {
