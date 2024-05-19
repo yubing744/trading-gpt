2ac8926fa8e7efb1d78ba5a00f5a26809ec93493
Owen Wu <yubing744@163.com>
2024-05-19 20:25:04 +0800

    feat: update prompt template

--- pkg/prompt/prompt.go
+++ pkg/prompt/prompt.go
@@ -19,10 +19,8 @@ Trading strategy points of attention:
 
 Performance Evaluation:
 1. Continuously review and analyze your actions to ensure you are performing to the best of your abilities.
-2. Constructively self-criticize your big-picture behavior constantly.
-3. Reflect on past decisions and strategies to refine your approach.
-4. Every command has a cost, so be smart and efficient. Aim to complete tasks in the least number of steps.
-5. Command parameters do not support variables or expressions. Please fill them in after calculating them step by step.
+2. Every command has a cost, so be smart and efficient. Aim to complete tasks in the least number of steps.
+3. Command parameters do not support variables or expressions. Please fill them in after calculating them step by step.
 
 Constraints:
 1. Exclusively use the commands listed in double quotes e.g. "command name"
@@ -30,6 +28,7 @@ Constraints:
 3、Be careful not to open positions repeatedly. If you need to adjust the take profit and stop loss, please use the exchange.update_position command.
 4、Please remember to only reply in JSON format, no other explanation is required.
 5、The returned JSON format does not support comments
+6、The analyze statement can be very long to ensure that the reasoning process of the analysis is rigorous.
 
 You should only respond in JSON format as described below 
 Response Format: 
